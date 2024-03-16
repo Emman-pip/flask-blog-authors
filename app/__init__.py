3fbc79e83d94f454131286dd234a5a73a9f5c9ea
Emman-pip <parrenoemmanuel756@gmail.com>
2024-03-16 11:28:54 +0800

    Make sqlite based application.

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,9 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import (
-    select,
-    Integer,
-    func,
-    Column,
-    Integer,
-    String,
-    ForeignKey,
-    text,
-    Text,
-)
 from flask_login import LoginManager
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, func, select, text
 
-from app import models, pages, auth
+from app import auth, models, pages
+
 from .authors import authors
 
 # class Base(DeclarativeBase):
@@ -26,7 +15,7 @@ from .authors import authors
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "Ssecret-key"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/blog"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     login_manager_author = LoginManager()
